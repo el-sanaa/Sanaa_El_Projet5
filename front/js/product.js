2f3a624c771c667de78e79362593f022cd283f1c
el-sanaa <sanaa00078@gmail.com>
2021-12-20 12:20:18 +0100

    Resoudre le probleme du local storage sur firefox

--- front/js/product.js
+++ front/js/product.js
@@ -1,20 +1,14 @@
-
-
-//router.get('/:id', productCtrl.getOneProduct);
+S//router.get('/:id', productCtrl.getOneProduct);
 //methode window.location.search
-//methode URLSearchParams pour extraire et passer l’id 
-//d’une page à une autre
-let searchId = [];
-
 const getInformation = window.location.search;
 console.log(getInformation);
 
+//methode constructor URLSearchParams pour extraire et passer l’id d’une page à une autre
 const kanapId = new URLSearchParams(getInformation);
 console.log(kanapId);
 
 const id = kanapId.get("id");
 console.log(id);
-
 
 //afficher les détails de l'id du produit 
 let selectedElements = [];
@@ -27,8 +21,7 @@ const fetchid = async () => {
             selectedElements = promise;
             console.log(selectedElements);
         });
-};
-
+}; 
 
 //Insérer le produit et ses détails dans la page produit
 const productsDisplay = async () => {
@@ -41,91 +34,93 @@ const productsDisplay = async () => {
     document.getElementById("description").innerHTML = `
           ${selectedElements.description}`
 
-
-const item__img = document.getElementById("img").innerHTML = `
-<img src="${selectedElements.imageUrl}" alt="${selectedElements.altTxt}">`;
+    const item__img = document.getElementById("img").innerHTML = `
+    <img src="${selectedElements.imageUrl}" alt="${selectedElements.altTxt}">`;
     console.log(item__img);
 
-
-
-
-const colors = document.getElementById("colors");
+    const colors = document.getElementById("colors");
     console.log(colors);
     console.log(selectedElements.colors);
 
-// Boucle for qui affiche toutes les options des couleurs
+    // Boucle for qui affiche toutes les options des couleurs
     for (let i = 0; i < selectedElements.colors.length; i++) {
         console.log(selectedElements.colors[i]);
         colors.innerHTML += `
     <option value = ${selectedElements.colors[i]}>${selectedElements.colors[i]}</option>`;
     };
 
-
- const quantity = document.getElementById("quantity");
+    let quantity = document.getElementById("quantity");
     console.log(quantity);
 
- 
-const button = document.getElementById("addToCart");
+    const button = document.getElementById("addToCart");
     console.log(button);
 
-//Ecouter l'event
+    //Ecouter l'event
     button.addEventListener("click", (e) => {
-        
-     event.preventDefault();
+        event.preventDefault();
         console.log(button);
 
+        const numberQuantity = quantity.value
+        console.log(numberQuantity);
 
-       
-    })
+        //Récuperer les valeurs  / Ajouter au panier
+        let optionsProduct = {
+            title: selectedElements.name,
+            img: selectedElements.imageUrl,
+            altTxt: selectedElements.altTxt,
+            id: id,
+            price: selectedElements.price,
+            colors: selectedElements.colors,
+            quantity: numberQuantity
+        }
+        console.log(optionsProduct);
 
- //Récuperer les valeurs  / Ajouter au panier
-    let chosenProduct = {
-        title: selectedElements.name,
-        img: selectedElements.imageUrl,
-        id: id,
-        price: selectedElements.price,
-        colors: selectedElements.colors,
-        quantity: 1
-    }
-    console.log(chosenProduct);
 
+        //Gestion du LocaleStorage--------------
+        //Stocker les valeurs du formulaire dans le local storage
+        //Stocker dans la variable Keys and Values mis dans le localstorage
+        //convertir données json (dans lecalstorage) en objet js
+        let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
+        console.log(productInLocalStorage);
+
+        //Function pour une fenêtre pop-up
+        //Methode window.confirm
+        const confirmation = () => {
+            if (window.confirm(`Votre commande de ${numberQuantity}
+            ${selectedElements.name} ${selectedElements.colors} est ajoute au panier
+            Pour consulter votre panier, cliquez sur OK`)) {
+                window.location.href = "cart.html";
+            }
+        }
+
+        //Function pour ajouter un produit selectione dans le local storage
+        const addProduct = () => {
+
+            //Ajouter le produit choisi dans localstorage
+            productInLocalStorage.push(optionsProduct);
+
+            //Ajouter les options et choix du client //Transformer en format JSON
+            localStorage.setItem("produit", JSON.stringify(productInLocalStorage));
+            console.log(productInLocalStorage);
+
+        }
+        // si il y a deja un produit enregistre dans le local storage
+        if (productInLocalStorage) {
+            addProduct();
+            console.log(productInLocalStorage);
+            confirmation();
+        }
+        //Sinon 
+        else {
+            productInLocalStorage = [];
+            addProduct();
+            console.log(productInLocalStorage);
+            confirmation();
+        }
+        
     
-//------------Locale Storage--------------
 
-//Stocker dans la variable Keys and Values 
-//convertir données json (dans lecalstorage) en objet js
-let productInLocalStorage = JSON.parse(localStorage.getItem("product"));
-console.log(productInLocalStorage);
+    });
 
-//Ajouter le produit choisi dans localstorage
-//Ajouter les options et choix du client //Transformer en format JSON
-const addProduct = () => {
-    productInLocalStorage.push(chosenProduct);
-    localStorage.setItem("product", JSON.stringify(productInLocalStorage));
 }
-
-//Si autres produits enregistrés dans le local storage
-if (productInLocalStorage) {
-    addProduct();
-    //popupConformation();
-       }
-
-//Sinon, produits enregistrés 
-else {
-    productInLocalStorage = [];
-    addProduct();
-    //popupConformation();
-       }
- }
-
 productsDisplay();
-
-
-
-
-
-
-
-
-
-
